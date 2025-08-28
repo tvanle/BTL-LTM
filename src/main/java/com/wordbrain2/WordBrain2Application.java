2fa13a3abaa84534cf83edc1d64490ebde5317ae
Trong Le <trongthptdqh@gmail.com>
2025-08-28 10:17:29 +0700

    Enable asynchronous processing in the application; add @EnableAsync annotation for improved performance.

--- src/main/java/com/wordbrain2/WordBrain2Application.java
+++ src/main/java/com/wordbrain2/WordBrain2Application.java
@@ -3,13 +3,15 @@ package com.wordbrain2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 @EnableScheduling
 public class WordBrain2Application {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(WordBrain2Application.class, args);
     }
