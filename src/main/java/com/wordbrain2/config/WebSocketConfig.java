5c7e560fdc4c25c293d55722469df0be9836c0d7
Trong Le <110284635+TVanLe@users.noreply.github.com>
2025-09-05 11:06:56 +0700

    remove unused

--- src/main/java/com/wordbrain2/config/WebSocketConfig.java
+++
@@ -1,8 +0,0 @@
-package com.wordbrain2.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebSocketConfig {
-    // TCP Socket configuration is handled automatically by GameWebSocketHandler
-}
